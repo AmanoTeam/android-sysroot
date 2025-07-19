b7f9c0ce6ac9c07d44c7446669ffc626220c1d96
Kartatz <105828205+Kartatz@users.noreply.github.com>
2025-07-19 15:26:12 -0300

    Add mips system roots

--- main.sh
+++ main.sh
@@ -8,9 +8,17 @@ declare -ra targets=(
 	'arm-unknown-linux-androideabi'
 	'x86_64-unknown-linux-android'
 	'i686-unknown-linux-android'
+	'mipsel-unknown-linux-android'
+	'mips64el-unknown-linux-android'
 )
 
 declare -r versions=(
+	'14'
+	'15'
+	'16'
+	'17'
+	'18'
+	'19'
 	'21'
 	'22'
 	'23'
@@ -32,10 +40,69 @@ declare -r pkg_file='/tmp/pkg.deb'
 
 declare -r ndk_archive='/tmp/ndk.zip'
 declare -r ndk_directory='/tmp/android-ndk-r27c'
+declare -r unsupported_ndk_directory='/tmp/android-ndk-r16b'
+
+declare -r workdir="${PWD}"
+
+function get_arch() {
+	
+	if [ "${1}" = 'aarch64-unknown-linux-android' ]; then
+		echo 'arm64'
+	fi
+	
+	if [ "${1}" = 'riscv64-unknown-linux-android' ]; then
+		echo 'riscv64'
+	fi
+	
+	if [ "${1}" = 'arm-unknown-linux-androideabi' ]; then
+		echo 'arm'
+	fi
+	
+	if [ "${1}" = 'x86_64-unknown-linux-android' ]; then
+		echo 'x86_64'
+	fi
+	
+	if [ "${1}" = 'i686-unknown-linux-android' ]; then
+		echo 'x86'
+	fi
+	
+	if [ "${1}" = 'mipsel-unknown-linux-android' ]; then
+		echo 'mips'
+	fi
+	
+	if [ "${1}" = 'mips64el-unknown-linux-android' ]; then
+		echo 'mips64'
+	fi
+
+}
+
+function remove_symbols() {
+
+	objcopy \
+		--redefine-sym '__stack_chk_fail=liaf_khc_kcats__' \
+		--redefine-sym '__stack_chk_fail_local=lacol_liaf_khc_kcats__' \
+		"${1}"
+	
+}
 
 if ! [ -f "${ndk_archive}" ]; then
 	curl \
 		--url 'https://dl.google.com/android/repository/android-ndk-r27c-linux.zip' \
+		--retry '30' \
+		--retry-all-errors \
+		--retry-delay '0' \
+		--retry-max-time '0' \
+		--location \
+		--silent \
+		--output "${ndk_archive}"
+	
+	unzip \
+		-d "$(dirname "${ndk_directory}")" \
+		-q \
+		"${ndk_archive}"
+	
+	curl \
+		--url 'https://dl.google.com/android/repository/android-ndk-r16b-linux-x86_64.zip' \
 		--retry '30' \
 		--retry-all-errors \
 		--retry-delay '0' \
@@ -86,14 +153,20 @@ if ! [ -f "${pkg_file}" ]; then
 fi
 
 for target in "${targets[@]}"; do
+	declare arch="$(get_arch ${target})"
 	declare triplet="${target/-unknown/}"
+	declare unsupported_ndk='0'
 	
 	for version in "${versions[@]}"; do
-		echo "${target}${version}"
-		
-		if ! [ -d "${ndk_directory}/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/${triplet}/${version}" ]; then
+		if [ -d "${ndk_directory}/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/${triplet}/${version}" ]; then
+			unsupported_ndk='0'
+		elif [ -d "${unsupported_ndk_directory}/platforms/android-${version}/arch-${arch}" ]; then
+			unsupported_ndk='1'
+		else
 			continue
 		fi
+		
+		echo "${target}${version}"
 		
 		declare sysroot_directory="/tmp/${target}${version}"
 		
@@ -101,9 +174,17 @@ for target in "${targets[@]}"; do
 		
 		mkdir --parent "${sysroot_directory}/lib"
 		
+		declare include_directory=''
+		
+		if (( unsupported_ndk )); then
+			include_directory="${unsupported_ndk_directory}/sysroot/usr/include"
+		else
+			include_directory='/tmp/data/data/com.termux/files/usr/include'
+		fi
+		
 		cp \
 			--recursive \
-			'/tmp/data/data/com.termux/files/usr/include' \
+			"${include_directory}" \
 			"${sysroot_directory}"
 		
 		cd "${sysroot_directory}/include"
@@ -117,10 +198,39 @@ for target in "${targets[@]}"; do
 			mv "${name}/"* './' && rmdir "${name}"
 		done
 		
-		cp \
-			"${ndk_directory}/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/${triplet}/${version}/"* \
-			"${ndk_directory}/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/${triplet}/"*.{a,so} \
-			"${sysroot_directory}/lib"
+		declare library_directory=''
+		declare library_directory2=''
+		
+		if (( unsupported_ndk )); then
+			library_directory="${unsupported_ndk_directory}/platforms/android-${version}/arch-${arch}/usr/lib"
+			library_directory2="${unsupported_ndk_directory}/platforms/android-${version}/arch-${arch}/usr/lib64"
+		else
+			library_directory="${ndk_directory}/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/${triplet}/${version}"
+			library_directory2="${ndk_directory}/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/${triplet}"
+		fi
+		
+		if (( unsupported_ndk )); then
+			cp \
+				"${library_directory}/"* \
+				"${library_directory2}/"* \
+				"${sysroot_directory}/lib" || true
+		else
+			cp \
+				"${library_directory}/"* \
+				"${library_directory2}/"*.{a,so} \
+				"${sysroot_directory}/lib" || true
+		fi
+		
+		if (( unsupported_ndk )); then
+			remove_symbols "${sysroot_directory}/lib/crtbegin_dynamic.o"
+			remove_symbols "${sysroot_directory}/lib/crtbegin_so.o"
+			remove_symbols "${sysroot_directory}/lib/crtbegin_static.o"
+			
+			patch --directory="${sysroot_directory}/include" --strip='1' --input="${workdir}/patches/0001-Header-fixes.patch"
+		fi
+		
+		rm "${sysroot_directory}/lib/lib"{compiler,stdc++,c++}* || true
+		rm --force --recursive "${sysroot_directory}/include/c++"
 		
 		declare tarball_filename="${sysroot_directory}.tar.xz"
 		
